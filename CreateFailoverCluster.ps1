5119659783210e59058e766f946457af8c87bf05
Simon Davies <simongdavies@hotmail.com>
2015-04-23 10:03:49 +0100

    updated cluster script

--- CreateFailoverCluster.ps1
+++ CreateFailoverCluster.ps1
@@ -24,11 +24,38 @@ configuration CreateFailoverCluster
 
     )
 
-    Import-DscResource -ModuleName xComputerManagement, xFailOverCluster
+    Import-DscResource -ModuleName xComputerManagement, xFailOverCluster,CDisk,xActiveDirectory,XDisk
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
    
     Node localhost
     {
+
+        xWaitforDisk Disk2
+        {
+             DiskNumber = 2
+             RetryIntervalSec =$RetryIntervalSec
+             RetryCount = $RetryCount
+        }
+
+        cDiskNoRestart DataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = "F"
+        }
+
+        xWaitforDisk Disk3
+        {
+             DiskNumber = 3
+             RetryIntervalSec =$RetryIntervalSec
+             RetryCount = $RetryCount
+        }
+
+        cDiskNoRestart LogDisk
+        {
+            DiskNumber = 3
+            DriveLetter = "G"
+        }
+
         WindowsFeature FC
         {
             Name = "Failover-Clustering"
@@ -40,7 +67,26 @@ configuration CreateFailoverCluster
             Name = "RSAT-Clustering-PowerShell"
             Ensure = "Present"
         }
-     
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+        }
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+        }
+          
         xCluster FailoverCluster
         {
             Name = $ClusterName
